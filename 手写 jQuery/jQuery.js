0b0fc54e0c9cdff7032b3a1aebc4c7f64692dc88
gzy <283211498@qq.com>
2020-02-02 20:00:33 +0800

    实现 end()

--- 手写 jQuery/jQuery.js
+++ 手写 jQuery/jQuery.js
@@ -7,6 +7,7 @@ window.jQuery = function (selectorOrArray) {
   }
   // api 对象，操作 elements
   return {
+    oldApi: selectorOrArray.oldApi,
     // 闭包&链式操作
     // 闭包：函数访问外部的变量
     addClass(className) {
@@ -20,7 +21,11 @@ window.jQuery = function (selectorOrArray) {
       for (let i = 0; i < elements.length; i++) {
         array = array.concat(Array.from(elements[i].querySelectorAll(selector)))
       }
+      array.oldApi = this   // 储存了旧的api，即string的时候
       return jQuery(array)
+    },
+    end(){
+      return this.oldApi    // 此时一般刚调用完Array，所以又回到了最初的this
     }
   }
 }
--- 手写 jQuery/main.js
+++ 手写 jQuery/main.js
@@ -1,5 +1,12 @@
 jQuery('.test-1').addClass('c1').addClass('c2')
 
-jQuery('.test-1').find('.test-2').addClass('c3')
+jQuery('.test-1')
+    .find('.test-2')
+    .addClass('c3') // 在子元素上
+    .end()
+    .addClass('c4') // 在父元素上
+
+
+
 
 
