9812186f30d336b5c4c5cd0dc0dd6f1a23ed72e6
gzy <283211498@qq.com>
2020-02-03 12:17:21 +0800

    实现 each、parent、children

--- 手写 jQuery/jQuery.js
+++ 手写 jQuery/jQuery.js
@@ -24,8 +24,33 @@ window.jQuery = function (selectorOrArray) {
       array.oldApi = this   // 储存了旧的api，即string的时候
       return jQuery(array)
     },
-    end(){
+    end() {
       return this.oldApi    // 此时一般刚调用完Array，所以又回到了最初的this
+    },
+    each(fn) {
+      for (let i = 0; i < elements.length; i++) {
+        fn.call(null, elements[i], i)
+      }
+      return this
+    },
+    parent() {
+      let array = []
+      this.each((node) => {
+        if (array.indexOf(node.parentNode) === -1) {
+          array.push(node.parentNode)  //  只有一个的时候push进去
+        }
+      })
+      return jQuery(array)
+    },
+    children() {
+      let array = []
+      this.each((node) => {
+        array.push(...node.children)  //  展开children
+      })
+      return jQuery(array)
+    },
+    print() {
+      console.log(elements)
     }
   }
 }
--- 手写 jQuery/main.js
+++ 手写 jQuery/main.js
@@ -6,7 +6,15 @@ jQuery('.test-1')
     .end()
     .addClass('c4') // 在父元素上
 
+jQuery('.test-2')
+    .each((div, i)=>{
+      console.log(i)
+      console.log(div)
+    })
 
+jQuery('.test-2')
+    .parent().print()
 
-
+jQuery('.test-1')
+    .children().print()
 
